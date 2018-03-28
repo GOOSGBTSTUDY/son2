4f7ba18b388b08a2caae5e932415a681c0b9d479
손현태 <htson@woowahan.com>
2018-03-28 12:43:20 +0900

    서버접속 구현 - 현태

--- src/main/java/auctionsniper/Main.java
+++ src/main/java/auctionsniper/Main.java
@@ -1,5 +1,11 @@
 package auctionsniper;
 
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
@@ -9,6 +15,14 @@ public class Main {
     public static final String MAIN_WINDOW_NAME = "main window name";
     public static final String STATUS_LOST = "status_lost";
     public static final String STATUS_JOINING = "joining...";
+    private static final int ARG_HOSTNAME = 0;
+    private static final int ARG_USERNAME = 1;
+    private static final int ARG_PASSWORD = 2;
+    private static final int ARG_ITEM_ID = 3;
+
+    private static final String AUCTION_RESOURCE = "Auction";
+    private static final String ITEM_ID_AS_LOGIN = "auction-%s";
+    private static final String AUCTION_ID_FORMAT = ITEM_ID_AS_LOGIN + "@%s/" + AUCTION_RESOURCE;
 
     private MainWindow ui;
 
@@ -18,6 +32,28 @@ public class Main {
 
     public static void main(String... args) throws Exception {
         Main main = new Main();
+
+        XMPPConnection connection = connectTo(args[ARG_HOSTNAME], args[ARG_USERNAME], args[ARG_PASSWORD]);
+
+        connection.getChatManager().createChat(auctionId(args[ARG_ITEM_ID], connection),
+                new MessageListener() {
+                    @Override
+                    public void processMessage(Chat chat, Message message) {
+                        // TODO
+                    }
+                });
+    }
+
+    private static String auctionId(String itemId, XMPPConnection connection) {
+        return String.format(AUCTION_ID_FORMAT, itemId, connection.getServiceName());
+    }
+
+    private static XMPPConnection connectTo(String hostname, String username, String password) throws XMPPException {
+        XMPPConnection connection = new XMPPConnection(hostname);
+        connection.connect();
+        connection.login(username, password, AUCTION_RESOURCE);
+
+        return connection;
     }
 
     private void startUserInterface() throws Exception {
