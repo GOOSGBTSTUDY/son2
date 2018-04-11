c4c8bba8b75969f0e449421b5604d75800aec7a5
Inbon Kuh <inbon.kuh@woowahan.com>
2018-04-11 12:49:59 +0900

    테스트와 도메인로직 보강

--- src/main/java/auctionsniper/WordChecker.java
+++ src/main/java/auctionsniper/WordChecker.java
@@ -4,6 +4,7 @@ public class WordChecker {
 
     public static final String RIGHT = "right word";
     public static final String WRONG = "wrong word";
+    public static final String UNKNOWN = "unknown word";
     private final String word;
 
     public WordChecker(String word) {
@@ -11,16 +12,38 @@ public class WordChecker {
     }
 
     public String check() {
+
         if (word.length() == 1) {
             return RIGHT;
         }
-        if (word.equals("12")) {
-            return RIGHT;
-        }
-        if (word.equals("21")) {
-            return RIGHT;
+
+        String currentStatus = RIGHT;
+
+        for (int i = 1; i < word.length(); i++) {
+            char prev = word.charAt(i - 1);
+            final char cur = word.charAt(i);
+
+            if (prev == cur) return WRONG;
+
+            if (cur != '?') continue;
+
+            if (i + 1 == word.length()) return UNKNOWN;
+
+            if (prev == word.charAt(i + 1)) currentStatus = UNKNOWN;
         }
 
-        return WRONG;
+        return currentStatus;
+
+//        if (word.equals("12")) {
+//            return RIGHT;
+//        }
+//        if (word.equals("21")) {
+//            return RIGHT;
+//        }
+//        if (word.equals("1?1")) {
+//            return UNKNOWN;
+//        }
+//
+//        return WRONG;
     }
 }
--- src/test/java/auctionsniper/WordCheckerTest.java
+++ src/test/java/auctionsniper/WordCheckerTest.java
@@ -2,55 +2,84 @@ package auctionsniper;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static auctionsniper.WordChecker.*;
+import static org.junit.Assert.assertEquals;
 
 public class WordCheckerTest {
     @Test
     public void jaum_duplicated_wrong_word() {
         WordChecker wordChecker = new WordChecker("11");
 
-        assertEquals("wrong word", wordChecker.check());
+        assertEquals(WRONG, wordChecker.check());
     }
 
     @Test
     public void moum_duplicated_wrong_word() {
         WordChecker wordChecker = new WordChecker("22");
 
-        assertEquals("wrong word", wordChecker.check());
+        assertEquals(WRONG, wordChecker.check());
     }
 
     @Test
     public void jaum_moum_right_word() {
         WordChecker wordChecker = new WordChecker("12");
 
-        assertEquals("right word", wordChecker.check());
+        assertEquals(RIGHT, wordChecker.check());
     }
 
     @Test
     public void moum_jaum_right_word() {
         WordChecker wordChecker = new WordChecker("21");
 
-        assertEquals("right word", wordChecker.check());
+        assertEquals(RIGHT, wordChecker.check());
     }
 
     @Test
     public void questionmark_right_word() {
         WordChecker wordChecker = new WordChecker("?");
 
-        assertEquals("right word", wordChecker.check());
+        assertEquals(RIGHT, wordChecker.check());
     }
 
     @Test
     public void moum_one_char_right_word() {
         WordChecker wordChecker = new WordChecker("1");
 
-        assertEquals("right word", wordChecker.check());
+        assertEquals(RIGHT, wordChecker.check());
     }
 
     @Test
     public void jaum_one_char_right_word() {
         WordChecker wordChecker = new WordChecker("2");
 
-        assertEquals("right word", wordChecker.check());
+        assertEquals(RIGHT, wordChecker.check());
+    }
+
+    @Test
+    public void jaum_q_jaum_unknown_word() {
+        WordChecker wordChecker = new WordChecker("1?1");
+
+        assertEquals(UNKNOWN, wordChecker.check());
+    }
+
+    @Test
+    public void jaum_q_jaum_jaum_wrong_word() {
+        WordChecker wordChecker = new WordChecker("1?11");
+
+        assertEquals(WRONG, wordChecker.check());
+    }
+
+    @Test
+    public void jaum_moum_jaum_right_word() {
+        final WordChecker wordChecker = new WordChecker("121");
+
+        assertEquals(RIGHT, wordChecker.check());
+    }
+
+    @Test
+    public void last_q_unknown_word() {
+        final WordChecker wordChecker = new WordChecker("1212?");
+
+        assertEquals(UNKNOWN, wordChecker.check());
     }
 }
